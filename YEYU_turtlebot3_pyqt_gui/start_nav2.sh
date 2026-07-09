4d042c94d04db187a9ea48a13bdbce829b02aa19
asejyooiw6ipk7ce <asejyooiw6ipk7ce@gmail.com>
2026-07-09 22:35:31 +0900

    map옵션 추가

--- YEYU_turtlebot3_pyqt_gui/start_nav2.sh
+++ YEYU_turtlebot3_pyqt_gui/start_nav2.sh
@@ -9,10 +9,10 @@ source /opt/ros/humble/setup.bash
 source ~/turtlebot3_ws/install/setup.bash
 
 export TURTLEBOT3_MODEL=burger
-export ROS_DOMAIN_ID=200
+export ROS_DOMAIN_ID=40
 export LDS_MODEL=LDS-03
 
-ros2 launch turtlebot3_navigation2 navigation2.launch.py 
+ros2 launch turtlebot3_navigation2 navigation2.launch.py map:=/home/ktel/pyqt_ws/src/YEYU_turtlebot3_pyqt_gui/config/yeyu_waypoints2.yaml
 ' > "$LOG_FILE" 2>&1 &
 
 echo $! > "$PID_FILE"
