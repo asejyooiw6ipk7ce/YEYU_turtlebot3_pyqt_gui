d112b02105b757de6d930a86ee63ed7228138353
asejyooiw6ipk7ce <asejyooiw6ipk7ce@gmail.com>
2026-07-09 22:52:49 +0900

    오타 실수: mapyaml이 아니라 waypoint yamll으로 썼었음

--- YEYU_turtlebot3_pyqt_gui/start_nav2.sh
+++ YEYU_turtlebot3_pyqt_gui/start_nav2.sh
@@ -12,7 +12,7 @@ export TURTLEBOT3_MODEL=burger
 export ROS_DOMAIN_ID=40
 export LDS_MODEL=LDS-03
 
-ros2 launch turtlebot3_navigation2 navigation2.launch.py map:=/home/ktel/pyqt_ws/src/YEYU_turtlebot3_pyqt_gui/config/yeyu_waypoints2.yaml
+ros2 launch turtlebot3_navigation2 navigation2.launch.py map:=/home/ktel/pyqt_ws/src/YEYU_turtlebot3_pyqt_gui/maps/yeyu_map1.yaml
 ' > "$LOG_FILE" 2>&1 &
 
 echo $! > "$PID_FILE"
